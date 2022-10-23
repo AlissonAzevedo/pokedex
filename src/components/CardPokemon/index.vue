454959641ecbfefa891bdf04dc1fd15031b5c513
AlissonAzevedo <61812126+AlissonAzevedo@users.noreply.github.com>
2022-10-23 20:47:18 -0300

    feat: add page home

---
+++ src/components/CardPokemon/index.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="container">
+        <div class="card">
+          <img :src="`https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`" :alt="pokemon.name" class="img-pokemon">
+          <p class="id">#{{pokemon.id}}</p>
+          <h3 class="title">{{pokemon.name}}</h3>
+          <router-link :to="`/pokemon/${pokemon.name}`">
+            <button class="btn">Ver detalhes</button>
+          </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+import {computed} from 'vue'
+export default {
+    name: 'CardPokemon',
+    props: {
+        pokemon: {
+            type: Object,
+            default: () => {}
+        }
+    },
+    setup () {
+        const pokemonInfo = computed(() => {
+            return this.pokemon
+        })
+        return {
+            pokemonInfo
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 1em;
+}
+.card {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 2em 1em;
+    margin-bottom: 2em;
+    width: 22%;
+    border-radius: 1em;
+    border: 1px solid #e33d33;
+    box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.5);
+    background-color: #333;
+    color: #fff;
+    min-width: 170px;
+}
+
+.img-pokemon {
+    width: 100%;
+    height: 100%;
+    max-width: 120px;
+    max-height: 120px;
+}
+
+.id {
+    background-color: #e33d33;
+    margin: 1em 0;
+    border-radius: 5px;
+    padding: 5px;
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.title {
+    text-transform: capitalize;
+    margin-bottom: 1em;
+    font-size: 1.3em;
+}
+
+.btn {
+    text-decoration: none;
+    background-color: #fff;
+    color: #222;
+    padding: 0.7em 1.2em;
+    border-radius: 6px;
+    font-weight: bold;
+    transition: 0.4s;
+}
+
+.btn:hover {
+    background-color: #e33d33;
+    color: #fff;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,155 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container-search" v-if="isSearch">
+      <img src="../assets/pokemon.png" alt="" class="logo">
+      <div class="search">
+        <input 
+          type="text" 
+          class="input" 
+          placeholder="Digite a busca aqui"
+          v-model="search">
+  
+          <button class="btn-search" @click="getPokemonbyName">Pesquisar</button>
+      </div>
+    </div>
+    <CardPokemon :pokemon="pokemon" v-if="isSearch"/>
+    <div class="container-search2" v-if="!isSearch">
+      <img src="../assets/pokemon.png" alt="" class="logo">
+      <div class="search">
+        <input 
+          type="text" 
+          class="input" 
+          placeholder="Digite a busca aqui"
+          v-model="search">
+  
+          <button class="btn-search" @click="getPokemonbyName">Pesquisar</button>
+      </div>
+    </div>
+    <!-- <CardPokemon :pokemon="pokemon" /> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {getPokemon} from '../../services/pokemon.service'
+import { ref, } from 'vue'
+import CardPokemon from '@/components/CardPokemon'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    CardPokemon
+  },
+  setup () {
+    const search = ref('')
+    const pokemon = ref({})
+    const isSearch = ref(false)
+    const getPokemonbyName = async () => {
+      const response = await getPokemon(search.value)
+      pokemon.value = response.data
+      isSearch.value = true
+      // console.log(pokemon.value)
+    }
+    return {
+      pokemon,
+      search,
+      getPokemonbyName,
+      isSearch
+    }
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.container-search{
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1em;
+  padding: 0 1em;
+
+  & .logo{
+    width: 300px;
+    margin-bottom: 1em;
+  }
+}
+.container-search2{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & .search{
+    width: 100%;
+    justify-content: space-around;
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+  }
+
+  & .logo{
+    width: 300px;
+    margin-bottom: 1em;
+  }
+
+}
+.input {
+  width: 80%;
+  padding: 1em;
+  border: 1px solid #e33d33;
+  border-radius: 6px;
+}
+.btn-search{
+  width: 15%;
+  padding: 1em;
+  border: 1px solid #e33d33;
+  border-radius: 6px;
+  background-color: #e33d33;
+  color: #fff;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+@media screen and (max-width: 768px) {
+  .container-search{
+    flex-direction: column;
+    width: 80%;
+    & .search{
+      flex-direction: column;
+
+      & .input{
+        margin-bottom: 1em;
+      }
+    }
+  }
+  .container-search2{
+    flex-direction: column;
+    width: 80%;
+    & .search{
+      flex-direction: column;
+
+      & .input{
+        margin-bottom: 1em;
+      }
+    }
+  }
+  .input {
+    width: 100%;
+  }
+  .btn-search{
+    width: 100%;
+  }
+  
+}
+</style>
