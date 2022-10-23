8e78127766752bf5bead4f89d930fff7c4879f61
AlissonAzevedo <61812126+AlissonAzevedo@users.noreply.github.com>
2022-10-23 20:46:48 -0300

    style: style app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+  </div> -->
+  <div class="main-container">
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
 
-#nav {
+/* #nav {
   padding: 30px;
 }
 
@@ -26,5 +33,10 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+} */
+
+.main-container{
+  min-height:70vh;
+  width: 100%;
 }
 </style>
