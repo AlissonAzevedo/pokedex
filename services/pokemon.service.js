5b921aafad6596f23f6ba303f812e6d130958ff3
Alisson Azevedo <alissonazevedo13.1@gmail.com>
2022-10-24 19:50:52 -0300

    chore: add getPokemonEvolution and getIdPokemon

--- services/pokemon.service.js
+++ services/pokemon.service.js
@@ -4,3 +4,13 @@ export function getPokemon(name) {
   const response = api.get(`/pokemon/${name}`);
   return response
 }
+
+export function getPokemonEvolution(id) {
+  const response = api.get(`/evolution-chain/${id}`);
+  return response
+}
+
+export function getIdPokemon(name) {
+  const response = api.get(`/pokemon/${name}`);
+  return response
+}
